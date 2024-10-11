85826ff7a86e06db9f441c237f93f61c39ea20ed
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-10-11 14:33:16 +0300

    feat(api): custom error classes for 400, 401, 403, and 404 status errors
    Signed-off-by: Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>

--- apps/api/src/common/classes.ts
+++ apps/api/src/common/classes.ts
@@ -1,32 +1,52 @@
-import HttpStatusCodes from '@src/common/HttpStatusCodes';
-
+import HttpStatusCodes from "@src/common/HttpStatusCodes";
 
 /**
  * Error with status code and message.
  */
 export class RouteError extends Error {
+	public status: HttpStatusCodes;
 
-  public status: HttpStatusCodes;
+	public constructor(status: HttpStatusCodes, message: string) {
+		super(message);
+		this.status = status;
+	}
+}
 
-  public constructor(status: HttpStatusCodes, message: string) {
-    super(message);
-    this.status = status;
-  }
+export class BadRequestException extends RouteError {
+	constructor(message: string) {
+		super(HttpStatusCodes.BAD_REQUEST, message);
+	}
+}
+
+export class UnauthorizedException extends RouteError {
+	constructor(message: string) {
+		super(HttpStatusCodes.UNAUTHORIZED, message);
+	}
+}
+
+export class ForbiddenException extends RouteError {
+	constructor(message: string) {
+		super(HttpStatusCodes.FORBIDDEN, message);
+	}
+}
+
+export class NotFoundException extends RouteError {
+	constructor(message: string) {
+		super(HttpStatusCodes.NOT_FOUND, message);
+	}
 }
 
 /**
  * If route validation fails.
  */
 export class ValidationErr extends RouteError {
+	public static MSG = 'The follow parameter were missing or invalid "';
 
-  public static MSG = 'The follow parameter were missing or invalid "';
+	public constructor(paramName: string) {
+		super(HttpStatusCodes.BAD_REQUEST, ValidationErr.GetMsg(paramName));
+	}
 
-  public constructor(paramName: string) {
-    super(HttpStatusCodes.BAD_REQUEST, ValidationErr.GetMsg(paramName));
-  }
-
-  public static GetMsg(param: string) {
-    return ValidationErr.MSG + param + '".';
-  }
+	public static GetMsg(param: string) {
+		return ValidationErr.MSG + param + '".';
+	}
 }
-
