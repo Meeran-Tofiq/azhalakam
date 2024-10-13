4efef33623808f3c61d2e2cc602a81b5363e27cd
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-10-13 16:24:26 +0300

    docs(api): specify the status code returned by each subclass of RouteError
    Signed-off-by: Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>

--- apps/api/src/common/classes.ts
+++ apps/api/src/common/classes.ts
@@ -12,24 +12,40 @@ export class RouteError extends Error {
 	}
 }
 
+/**
+ * Status code: 400
+ * If request is invalid.
+ */
 export class BadRequestException extends RouteError {
 	constructor(message: string) {
 		super(HttpStatusCodes.BAD_REQUEST, message);
 	}
 }
 
+/**
+ * Status code: 401
+ * If user is not authenticated.
+ */
 export class UnauthorizedException extends RouteError {
 	constructor(message: string) {
 		super(HttpStatusCodes.UNAUTHORIZED, message);
 	}
 }
 
+/**
+ * Status code: 403
+ * If user is not allowed to perform an action.
+ */
 export class ForbiddenException extends RouteError {
 	constructor(message: string) {
 		super(HttpStatusCodes.FORBIDDEN, message);
 	}
 }
 
+/**
+ * Status code: 404
+ * If resource is not found.
+ */
 export class NotFoundException extends RouteError {
 	constructor(message: string) {
 		super(HttpStatusCodes.NOT_FOUND, message);
