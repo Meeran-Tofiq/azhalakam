f3334b67748dd0ec95cb0ad0446b2610ec903cc2
MohammedF02 <mhamadfriad4@gmail.com>
2024-10-14 01:09:53 +0300

    feat(native): add welcome screen to navigation

--- apps/native/src/navigation/Navigation.tsx
+++ apps/native/src/navigation/Navigation.tsx
@@ -2,15 +2,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import screen from '../screens/screen';
+import WelcomeScreen from '../screens/WelcomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={screen} />
+      <Stack.Navigator initialRouteName="Welcome" screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
