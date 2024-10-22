b62610f6ab81cc7db6525c928ffcb0e499c39cab
MohammedF02 <mhamadfriad4@gmail.com>
2024-10-22 21:09:23 +0300

    feat(native): add navigation to registration screen

--- apps/native/src/navigation/Navigation.tsx
+++ apps/native/src/navigation/Navigation.tsx
@@ -1,16 +1,18 @@
-// src/navigation/Navigation.tsx
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from '../screens/WelcomeScreen';
+import RegistrationScreen from '../screens/RegistrationScreen';
+import { RootStackParamList } from '../types/types';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Welcome" screenOptions={{headerShown: false}}>
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Registration" component={RegistrationScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
