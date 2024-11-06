9af50fa48789df718496736a1f2276f4430785a9
MohammedF02 <mhamadfriad4@gmail.com>
2024-11-06 22:18:37 +0300

    feat(native): add login and main page to navigation

--- apps/native/src/navigation/Navigation.tsx
+++ apps/native/src/navigation/Navigation.tsx
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from '../screens/WelcomeScreen';
 import RegistrationScreen from '../screens/RegistrationScreen';
+import LoginScreen from '../screens/LoginScreen';
+import MainPage from '../screens/MainPage';
 import { RootStackParamList } from '../types/types';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -13,6 +15,8 @@ const Navigation = () => {
       <Stack.Navigator initialRouteName="Welcome" screenOptions={{headerShown: false}}>
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Registration" component={RegistrationScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="MainPage" component={MainPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
