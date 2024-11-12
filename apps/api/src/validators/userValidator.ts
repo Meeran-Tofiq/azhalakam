1f62a87851dd6f95576290922abd15b2a0f89be4
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-11-12 17:54:19 +0300

    feat(api): made passwords easier to make

--- apps/api/src/validators/userValidator.ts
+++ apps/api/src/validators/userValidator.ts
@@ -12,11 +12,7 @@ export const createUserValidator = [
 		.escape()
 		.isString()
 		.isLength({ min: 3, max: 20 })
-		.withMessage("Username must be between 3 and 20 characters")
-		.matches(/^[a-zA-Z0-9_-]+$/)
-		.withMessage(
-			"Username can only contain letters, numbers, underscores, and hyphens."
-		),
+		.withMessage("Username must be between 3 and 20 characters"),
 	body("email")
 		.exists()
 		.trim()
