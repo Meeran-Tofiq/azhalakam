ba465e269c88927456d3040d9afecd61b0822c22
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-11-16 16:44:17 +0300

    fix(native): fix apiclient to return one instance for every use

--- apps/native/src/hooks/useApiClient.ts
+++ apps/native/src/hooks/useApiClient.ts
@@ -1,6 +1,8 @@
 import ApiClient from "../../../../packages/api-client/src/index";
 
+let apiClient: ApiClient;
+
 export default function useApiClient() {
-	const apiClient = new ApiClient(process.env.EXPO_PUBLIC_API_URL); // changed based on PC's IP for the expo go connection
+	if (!apiClient) apiClient = new ApiClient(process.env.EXPO_PUBLIC_API_URL); // changed based on PC's IP for the expo go connection
 	return apiClient;
 }
