031762415bfc67c8f59465c4c0b23510f79763b3
MohammedF02 <mhamadfriad4@gmail.com>
2024-11-20 20:57:19 +0300

    refactor(native): utilize components in store creation screen

--- apps/native/src/screens/StoreCreationScreen.tsx
+++ apps/native/src/screens/StoreCreationScreen.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { View, Text, StyleSheet, Alert, ScrollView } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
-import BackButton from "../components/BackButton";
 import FormInput from "../components/FormInput";
 import GenericButton from "../components/GenericButton";
 import useApiClient from "../hooks/useApiClient";
 import { useNavigation } from "@react-navigation/native";
 import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../types/types";
+import Header from "../components/Header";
 
 type FormValues = {
 	name: string;
@@ -42,7 +42,7 @@ const StoreCreationScreen = () => {
 				userId: "7b35cd8c-e53a-4f03-8291-0d443db73e8f",
 			};
 
-			const response = await apiClient.storeApi.createStore({
+			await apiClient.storeApi.createStore({
 				store: storeData,
 			});
 
@@ -56,9 +56,8 @@ const StoreCreationScreen = () => {
 
 	return (
 		<ScrollView contentContainerStyle={styles.container}>
-			<View style={styles.header}>
-				<BackButton />
-			</View>
+			<Header title="Create Store" />
+
 			<View style={styles.content}>
 				<Text style={styles.title}>Create Your Store</Text>
 
@@ -81,13 +80,21 @@ const StoreCreationScreen = () => {
 						render={({ field: { onChange, value } }) => (
 							<Picker
 								selectedValue={value}
-								onValueChange={(itemValue: "PET_STORE" | "VET_STORE") => {
+								onValueChange={(
+									itemValue: "PET_STORE" | "VET_STORE"
+								) => {
 									onChange(itemValue);
 								}}
 								style={styles.picker}
 							>
-								<Picker.Item label="Pet Store" value="PET_STORE" />
-								<Picker.Item label="Vet Store" value="VET_STORE" />
+								<Picker.Item
+									label="Pet Store"
+									value="PET_STORE"
+								/>
+								<Picker.Item
+									label="Vet Store"
+									value="VET_STORE"
+								/>
 							</Picker>
 						)}
 					/>
@@ -107,16 +114,11 @@ const styles = StyleSheet.create({
 	container: {
 		flexGrow: 1,
 		backgroundColor: "#F0F4F8",
-		padding: 20,
-	},
-	header: {
-		flexDirection: "row",
-		alignItems: "center",
-		marginTop: 20,
 	},
 	content: {
 		flex: 1,
 		marginTop: 40,
+		marginHorizontal: 20,
 	},
 	title: {
 		fontSize: 24,
@@ -141,7 +143,7 @@ const styles = StyleSheet.create({
 	},
 	submitButton: {
 		marginTop: 20,
-		backgroundColor: "#4652cc",
+		backgroundColor: "#4652CC",
 		padding: 15,
 		borderRadius: 5,
 		alignItems: "center",
