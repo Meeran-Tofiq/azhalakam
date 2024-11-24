441a3dc2e1462b8d3f6af06a7312b1654e7f57c4
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-11-24 15:01:03 +0300

    refactor(api): add exporting enums of prisma better

--- apps/api/src/types/PrismaEnums.ts
+++ apps/api/src/types/PrismaEnums.ts
@@ -3,3 +3,6 @@ import { $Enums } from "@prisma/client";
 export type Gender = $Enums.Gender;
 export type Species = $Enums.Species;
 export type StoreType = $Enums.StoreType;
+
+const Enums = $Enums;
+export default Enums;
