52544e88946c2956101be288e21d326864b39913
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-11-24 16:24:42 +0300

    faet(api): add review services with type validations

---
+++ apps/api/src/types/Review.ts
@@ -0,0 +1,39 @@
+import { Prisma, Review } from "@prisma/client";
+
+export type ReviewWithoutIncludes = Prisma.ReviewGetPayload<{}>;
+export type ReviewWithIncludes = Prisma.ReviewGetPayload<{
+	include: {};
+}>;
+
+// Create a review types
+
+export type CreateReviewInputs = {
+	review: Omit<
+		Prisma.ReviewCreateInput,
+		"id" | "store" | "user" | "product" | "serviceProvider"
+	>;
+	userId: string;
+};
+export type CreateReviewResponse = { review: ReviewWithIncludes };
+
+// Get review(s) types
+
+export type GetReviewInputs = {
+	id: string;
+};
+export type GetReviewResponse = { review: ReviewWithIncludes };
+
+// Update review types
+
+export type UpdateReviewInputs = {
+	id: string;
+	updateData: Omit<Prisma.ReviewUpdateInput, "id" | "store" | "user">;
+};
+export type UpdateReviewResponse = { review: ReviewWithIncludes };
+
+// Delete review types
+
+export type DeleteReviewInputs = {
+	id: string;
+};
+export type DeleteReviewResponse = { review: ReviewWithIncludes };
