5a1acd8fefde6b29c770036849e1fd8925f4f57c
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-12-05 17:12:11 +0300

    fix(api-client): fix getting all products with filtering options

--- packages/api-client/src/services/productApi.ts
+++ packages/api-client/src/services/productApi.ts
@@ -19,44 +19,26 @@ export default class ProductApi {
 		this.productUrl = baseUrl + "/products";
 	}
 
-	async getAllProductsAtPage({
-		page,
-	}: Omit<GetAllProductsInputs, "storeId">): Promise<GetAllProductsResponse> {
-		try {
-			const response = await fetch(`${this.productUrl}/all?page=${page}`);
-
-			if (!response.ok) {
-				throw new Error(response.statusText);
-			}
-
-			return await response.json();
-		} catch (error) {
-			throw error;
-		}
-	}
-
 	/**
-	 * Asynchronously fetches all products at a given page associated with a given store.
-	 * Requires the id of the store to be provided for the request.
-	 * @param page The page of products to fetch.
-	 * @param storeId The id of the store to get products from.
-	 * @throws {Error} If no store id is provided, if the request fails, or if the response is not ok.
+	 * Asynchronously fetches all products at a given page, optionally filtered by store ID and category.
+	 * @param page The page number of products to retrieve.
+	 * @param storeId (Optional) The ID of the store to filter products by.
+	 * @param category (Optional) The category to filter products by.
+	 * @throws {Error} If the request fails or if the response is not ok.
 	 * @returns A Promise that resolves with the JSON data of all products at the given page.
 	 */
-	async getAllProductsAtPageOfStore({
+	async getAllProductsAtPage({
 		page,
 		storeId,
 		category,
 	}: GetAllProductsInputs): Promise<GetAllProductsResponse> {
+		let queryParams = `page=${page}`;
+		if (storeId) queryParams += `&storeId=${storeId}`;
+		if (category) queryParams += `&category=${category}`;
+
 		try {
 			const response = await fetch(
-				`${this.productUrl}/all?page=${page}&storeId=${storeId}&category=${category}`,
-				{
-					method: "GET",
-					headers: {
-						"Content-Type": "application/json",
-					},
-				}
+				`${this.productUrl}/all?${queryParams}`
 			);
 
 			if (!response.ok) {
